581a8fca74081bf0eb3a30613b0b8634b1124021
Joseph Jones <josepjones25@gmail.com>
2017-06-01 20:42:02 -0500

    homework 4 complete sans ec

---
+++ hw4p1.js
@@ -0,0 +1,6 @@
+document.body.style.fontFamily = "sans-serif";
+document.body.style.backgroundColor = "red";
+document.getElementById("wow").style.backgroundColor = "blue";
+document.getElementById("nickname").innerHTML = "Joey";
+document.getElementById("favorites").innerHTML = "Pizza, Icecream";
+document.getElementById("hometown").innerHTML = "Jacksonville, Florida";
